ff94190e9cebbe63bc2e038bed4b58f0a5b81a71
Miss爱编程 <31149654+MissFreak@users.noreply.github.com>
2020-12-04 20:46:39 +0800

    Update plot.py

--- Plot/plot.py
+++ Plot/plot.py
@@ -1,4 +1,4 @@
-import matplotlib
+import matplotlib.pyplot as plt
 fig, ax = plt.subplots(figsize=[2,0.5])
 df["Label"].reset_index().groupby("Label").count().sort_values(by= 
        "index").plot(kind="barh", legend=False, 
